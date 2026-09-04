636f4009bf7e2d0656d1de4296bf0d4d2e57f23d
Shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-09-04 12:34:32 +0500

    correction and completion of second project

--- SecondProject_by_chaiCode.js
+++ SecondProject_by_chaiCode.js
@@ -1,8 +1,9 @@
 const form=document.querySelector('form')
 form.addEventListener('submit',function(e){
+  e.preventDefault()
   const weight=parseInt(document.querySelector('#weight').value);
   const height=parseInt(document.querySelector('#height').value)
-  const result=document.querySelector('#result')
+  const result=document.querySelector('#results')
   if (height === '' || height < 0 || isNaN(height)){
     result.textContent=`please give us valid height ${height}`;
   }else if (weight ===' ' || weight < 0 || isNaN(weight)){
@@ -10,5 +11,11 @@ form.addEventListener('submit',function(e){
   }else{
     const bmi=(weight/(height*height)).toFixed(2);
     result.textContent=`your bmi is ${bmi}`;
+    if (bmi<18)
+    {
+      result.innerHTML=`underweight ${bmi}`;
+    }
+    else{
+      result.innerHTML=`overweight ${bmi}`;}
   }
 })

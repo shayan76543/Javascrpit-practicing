a8ef5ac32c04048a84c7f4700d89b7e45fb57944
Shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-09-08 18:10:58 +0500

    update in forth project

--- Forth_Project.js
+++ Forth_Project.js
@@ -6,14 +6,49 @@ const remaningGuesses=document.querySelector('.lastResult');
 const ResultOfGuess=document.querySelector('.lowOrHi');
 let Guesses=[];
 let numberG=1;
+let playGame=true;
+if(playGame){
+  addEventListener('click',function(e){
+    e.preventDefault();
+    Validation(userInput);
+  })}
+else
+
+}
 
 function Validation(){
-  //
+  if (isNaN(userInput)){
+    alert('please enter Valid Number')
+  }else if (userInput<0){
+    alert('please Entere value above the Zero')
+  }else if(userInput>100){
+    alert('please Entere Value under the age 1-100 range')
+  }else{
+  guesses.push(userInput)
+  if (userInput===randomNumber){
+    CleanUp()
+    displayMessage(`Game over.random Number Was ${userInput}`)
+    endGame()
+  }
+  else{
+    CleanUp()
+    check()
+  }
+  } 
 }
-function check(){
+function check(userInput){
+  if (userInput===userInput){
+    displayMessage('Your Guess are correct.Congratulate....')
+    endGame()
+  }
+  else if (userInput>randomNumber){
+    displayMessage('Your Guess Number are to Much High....')
+  }else if (userInput<randomNumber){
+    displayMessage('Your Guess Number are to Much low....')
+  }
+  }
 
-}
-function displayGuess(){
+function CleanUp(userInput){
 
 }
 function displayMessage(){
